7eaa2cd3bcaba57c64abb2b5f03b3af316041fbb
RushilPatel0703 <30756824+RushilPatel0703@users.noreply.github.com>
2020-09-11 20:02:44 +0200

    favicon added, smooth scroll added, fixed typos and wording done

---
+++ vendors/js/test.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  var scrollLink = $('.scroll');
+
+  // Smooth scrolling
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000 );
+  });
+
+  // Active link switching
+
+
+})
